344057aaee6443616a1bf0a4e5d0c006020a9c54
기나윤 <nayun@ginayun-ui-MacBookAir.local>
2024-02-05 21:29:42 +0900

    fin: cpp06

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -10,23 +10,4 @@ public:
 	virtual ~Base();
 };
 
-class A : public Base {
-public:
-	~A();
-};
-
-class B : public Base {
-public:
-	~B();
-};
-
-class C : public Base {
-public:
-	~C();
-};
-
-Base * generate(void);
-void identify(Base* p);
-void identify(Base& p);
-
 #endif
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,4 +1,51 @@
-#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void) {
+	Base * ret;
+
+	srand(time(NULL));
+	if (rand() % 3 == 0)
+		ret = new A();
+	else if (rand() % 3 == 1)
+		ret = new B();
+	else // (rand() % 3 == 2)
+		ret = new C();
+	return (ret);
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Pointer of Base class" << std::endl;
+}
+
+void identify(Base& p) { // reference는 Null 없어서 실패시 std::bad_cast throw
+	try {
+		A a = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception & e) {
+	}
+	try {
+		B b = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception & e) {
+	}
+	try {
+		C c = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception & e) {
+	}
+}
 
 int	main() {
 	A a;
@@ -6,7 +53,6 @@ int	main() {
 	C c;
 	Base d;
 	Base *random = generate();
-
 
 	std::cout << "identification by pointer" << std::endl;
 	identify(&a);
@@ -19,9 +65,9 @@ int	main() {
 	
 	std::cout << "Identification by reference" << std::endl;
 	identify(a);
-	// identify(b);
-	// identify(c);
-	// identify(d);
+	identify(b);
+	identify(c);
+	identify(d);
 	identify(random);
 	
 	delete random;
