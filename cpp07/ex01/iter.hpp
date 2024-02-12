b8dd23f971497231e74cc1e832cdad08462fbdc0
naki <naki@student.42seoul.kr>
2024-02-12 15:05:53 +0900

    pause

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,24 +3,12 @@
 
 # include <iostream>
 
-template <typename T1, typename T2, typename T3>
-void iter(T1 addr, T2 len, T3 func) { // type T1, T2, T3 are different
-	for (int i = 0; i < len; i++) {
-		func(*addr);
-		addr++;
-	}
-}
-
-template <>
-void iter(int *addr, int len, int (*func)(int)) {
+template <typename T>
+void iter(T *addr, int len, void *func) {
 	for (int i = 0; i < len; i++) {
 		func(addr[i]);
+		addr++;
 	}
-}
-
-template <typename T>
-void plusOne(T &value) {
-	value++;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,17 +1,23 @@
 #include "iter.hpp"
 
-void function(int &a) {
-	a++;
+template <typename T>
+void plusOne(T &value) {
+	value++;
 }
-
+/*
+double plusOne(double &num) {
+	num++;
+	return (num);
+}
+*/
 int main( void ) {
 	int intArray[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	char charArray[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 	double doubleArray[10] = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09};
 
-	::iter(intArray, 10, function);
-	::iter(charArray, 10, function);
-	::iter(doubleArray, 10, function);
+	::iter(intArray, 10, plusOne);
+	::iter(charArray, 10, plusOne);
+	::iter(doubleArray, 10, plusOne);
 
     return 0;
 }
