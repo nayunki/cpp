e0d23a5321bbed4161db360cd40d4d96866473e7
기나윤 <nayun@ginayun-ui-MacBookAir.local>
2024-02-24 22:13:08 +0900

    fin: cpp08 ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,20 +6,27 @@
 # include <stack>
 
 template <typename T>
-class MutantStack {
-private:
-    std::stack<T> ms;
+class MutantStack : public std::stack<T> {
 public:
-    MutantStack();
-    MutantStack(const MutantStack & obj);
-    MutantStack & operator=(const MutantStack & obj);
-    ~MutantStack();
-    push(int num);
-    pop();
-    size();
-    top();
-    begin();
-    end();
+    MutantStack(){};
+    MutantStack(const MutantStack & obj): std::stack<T>(obj) {};
+    MutantStack & operator=(const MutantStack & obj){
+        *this = obj;
+        return *this;
+    };
+    ~MutantStack(){};
+    typedef typename std::stack<T>::container_type::iterator                iterator;
+    typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+	iterator begin(void) {return this->c.begin();}
+	iterator end(void) {return this->c.end();}
+	const_iterator cbegin(void) {return this->c.cbegin();}
+	const_iterator cend(void) {return this->c.cend();}
+	reverse_iterator rbegin(void) {return this->c.rbegin();}
+	reverse_iterator rend(void) {return this->c.rend();}
+	const_iterator crbegin(void) {return this->c.crbegin();}
+	const_iterator crend(void) {return this->c.crend();}
 };
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,8 +1,16 @@
 #include "MutantStack.hpp"
+#include <list>
+
+/*
+아래의 동일한 코드를 한 번은 MutantStack으로, 다른 한 번은 ex) list로 실행해서 동일한 결과가 나오는지 확인하라.
+서로 다른 컨테이너를 사용할 때에는 push()를 push_back()으로 바꾸는 등 적절한 멤버함수를 사용하라. 
+*/
 
 int main()
 {
     MutantStack<int> mstack;
+
+    std::cout << "------Test MutantStack begin-----" << std::endl;
 
     mstack.push(5);
     mstack.push(17);
@@ -27,11 +35,47 @@ int main()
 
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
 
     std::stack<int> s(mstack);
-    
+
+    std::cout << "------Test MutantStack end-----" << std::endl;
+    std::cout << "///////////////////////////////" << std::endl;
+    std::cout << "------Test std::list begin-----" << std::endl;
+
+    std::list<int> li;
+
+    li.push_back(5);
+    li.push_back(17);
+
+    std::cout << li.back() << std::endl;
+
+    li.pop_back();
+
+    std::cout << li.size() << std::endl;
+
+    li.push_back(3);
+    li.push_back(5);
+    li.push_back(737);
+    li.push_back(0);
+
+    std::list<int>::iterator itLi = li.begin();
+    std::list<int>::iterator iteLi = li.end();
+
+    ++itLi;
+    --itLi;
+
+    while (itLi != iteLi)
+    {
+        std::cout << *itLi << std::endl;
+        ++itLi;
+    }
+
+    std::list<int> sLi(li);
+
+    std::cout << "------Test std::list end-----" << std::endl;
+
     return 0;
 }
