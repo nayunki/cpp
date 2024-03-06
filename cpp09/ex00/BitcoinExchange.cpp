a785213442bf2b55d231a13d38d5ddae6066d0f8
naki <naki@student.42seoul.kr>
2024-03-06 18:34:53 +0900

    수강정정망했다

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,22 @@
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange & obj) {
+	*this = obj;
+}
+
+BitcoinExchange & BitcoinExchange::operator=(const BitcoinExchange & obj) {
+	(void)obj;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+const char *BitcoinExchange::invalidDatabase::what() const throw() {
+	return "Error: invalid database";
+}
+
+const char *BitcoinExchange::invalidInput::what() const throw() {
+	return "Error: invalid input";
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -2,7 +2,26 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
+# include <string>
+# include <cstdlib>
 # include <exception>
 # include <fstream>
+# include <map>
+# include <algorithm>
+
+class BitcoinExchange {
+private:
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange & obj);
+	BitcoinExchange & operator=(const BitcoinExchange & obj);
+	~BitcoinExchange();
+	class	invalidDatabase : public std::exception {
+		const char *what() const throw();
+	};
+	class	invalidInput : public std::exception {
+		const char *what() const throw();
+	};
+};
 
 #endif
